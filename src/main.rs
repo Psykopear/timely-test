1cca50fa94bc0db35e1eadd12efb95775d3b7252
Federico Dolce <psykopear@gmail.com>
2022-08-02 17:55:19 +0200

    Using input operator for dir entries

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,16 @@
 mod dirutils;
 
-use std::{env, fs, path::PathBuf, collections::hash_map::DefaultHasher};
 use std::hash::{Hash, Hasher};
+use std::{collections::hash_map::DefaultHasher, env, fs, path::PathBuf};
 
+use timely::dataflow::channels::pact::Pipeline;
+use timely::dataflow::operators::generic::source;
+use timely::dataflow::operators::Operator;
 use timely::dataflow::{
-    operators::{Exchange, Filter, Input, Inspect, Map, Probe},
+    operators::{aggregation::Aggregate, Exchange, Filter, Input, Inspect, Map, Probe},
     InputHandle,
 };
 use walkdir::{DirEntry, WalkDir};
-
 
 fn calculate_hash<T: Hash>(t: &T) -> u64 {
     let mut s = DefaultHasher::new();
@@ -19,58 +21,53 @@ fn calculate_hash<T: Hash>(t: &T) -> u64 {
 fn main() {
     timely::execute_from_args(env::args(), |worker| {
         let index = worker.index();
-        let peers = worker.peers();
-        let mut input = InputHandle::new();
 
         worker.dataflow::<u64, _, _>(|scope| {
-            scope
-                .input_from(&mut input)
-                // .filter(move |(i, _entry)| i % peers == index)
-                // .map(|(_i, entry)| entry)
-                .exchange(|entry: &PathBuf| {
-                    let entry_string = entry.as_os_str().to_str().unwrap().to_string();
-                    calculate_hash(&entry_string)
-                })
-                .map(dirutils::SearchResult::try_from)
-                .filter(Result::is_ok)
-                .map(Result::unwrap)
-                .map(dirutils::SearchResult::with_icon)
-                .inspect(|search_result| {
-                    println!("{} - {:?}", search_result.name, search_result.icon_path)
-                })
-                .probe()
-        });
+            source(scope, "Dirutils", |cap, info| {
+                let mut cap = Some(cap);
+                move |output| {
+                    if let Some(cap) = cap.as_mut() {
+                        let time = cap.time().clone();
 
-        if index == 0 {
-            let mut i = 0;
-            for folder in dirutils::search_dirs().iter() {
-                for entry in WalkDir::new(folder)
-                    .into_iter()
-                    .filter_entry(|e| !dirutils::is_hidden(e))
-                    .filter_map(Result::ok)
-                    .filter(dirutils::is_desktop_file)
-                    .map(DirEntry::into_path)
-                {
-                    input.send(entry);
-                    input.advance_to(i as u64 + 1);
-                    worker.step();
-                    i += 1;
-                }
-            }
+                        if index == 0 {
+                            for folder in dirutils::search_dirs().iter() {
+                                output.session(&cap).give_iterator(
+                                    WalkDir::new(folder)
+                                        .into_iter()
+                                        .filter_entry(|e| !dirutils::is_hidden(e))
+                                        .filter_map(Result::ok)
+                                        .filter(dirutils::is_desktop_file)
+                                        .map(DirEntry::into_path),
+                                );
+                            }
 
-            if let Some(paths) = env::var_os("PATH") {
-                for path in env::split_paths(&paths) {
-                    if let Ok(entries) = fs::read_dir(path) {
-                        for entry in entries.filter_map(Result::ok).map(|e| e.path()) {
-                            input.send(entry);
-                            input.advance_to(i as u64 + 1);
-                            worker.step();
-                            i += 1;
+                            if let Some(paths) = env::var_os("PATH") {
+                                for path in env::split_paths(&paths) {
+                                    if let Ok(entries) = fs::read_dir(path) {
+                                        output.session(&cap).give_iterator(
+                                            entries.filter_map(Result::ok).map(|e| e.path()),
+                                        );
+                                    }
+                                }
+                            }
                         }
                     }
+                    cap = None;
                 }
-            }
-        }
+            })
+            .exchange(|entry: &PathBuf| {
+                let entry_string = entry.as_os_str().to_str().unwrap().to_string();
+                calculate_hash(&entry_string)
+            })
+            .map(dirutils::SearchResult::try_from)
+            .filter(Result::is_ok)
+            .map(Result::unwrap)
+            .map(dirutils::SearchResult::with_icon)
+            .inspect(|search_result| {
+                println!("{} - {:?}", search_result.name, search_result.icon_path)
+            })
+            .probe()
+        });
     })
     .unwrap();
 }
