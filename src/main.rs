bf16e83b8b3966d472e6b922367cd1e4f069d619
Federico Dolce <psykopear@gmail.com>
2022-08-03 09:13:27 +0200

    Trying to add user input

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,8 @@
 mod dirutils;
 
 use std::hash::{Hash, Hasher};
+use std::thread;
+use std::time::Duration;
 use std::{collections::hash_map::DefaultHasher, env, fs, path::PathBuf};
 
 use timely::dataflow::channels::pact::Pipeline;
@@ -21,14 +23,13 @@ fn calculate_hash<T: Hash>(t: &T) -> u64 {
 fn main() {
     timely::execute_from_args(env::args(), |worker| {
         let index = worker.index();
+        let mut user_input = InputHandle::<u64, String>::new();
 
         worker.dataflow::<u64, _, _>(|scope| {
-            source(scope, "Dirutils", |cap, info| {
+            source(scope, "Dirutils", |cap, _info| {
                 let mut cap = Some(cap);
                 move |output| {
                     if let Some(cap) = cap.as_mut() {
-                        let time = cap.time().clone();
-
                         if index == 0 {
                             for folder in dirutils::search_dirs().iter() {
                                 output.session(&cap).give_iterator(
@@ -63,11 +64,51 @@ fn main() {
             .filter(Result::is_ok)
             .map(Result::unwrap)
             .map(dirutils::SearchResult::with_icon)
+            .binary(
+                &user_input.to_stream(scope),
+                Pipeline,
+                Pipeline,
+                "UserInput",
+                |cap, _info| {
+                    let mut cap = Some(cap);
+
+                    move |results, user_input, output| {
+                        let mut query_string = None;
+                        while let Some((_time, data)) = user_input.next() {
+                            for string in data.iter() {
+                                println!("==> {}", string);
+                                query_string = Some(string.clone());
+                            }
+                        }
+                        if let Some(qs) = query_string {
+                            // println!("qs: {}", qs);
+
+                            if let Some(cap) = cap.take() {
+                                while let Some((_time, data)) = results.next() {
+                                    for sr in data.iter() {
+                                        if sr.name.to_lowercase().contains(&qs) {
+                                            output.session(&cap).give(sr.clone());
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                    }
+                },
+            )
             .inspect(|search_result| {
                 println!("{} - {:?}", search_result.name, search_result.icon_path)
             })
             .probe()
         });
+
+        user_input.send("firefox".to_string());
+        // user_input.advance_to(1u64);
+        worker.step();
+        thread::sleep(Duration::from_secs(1));
+        user_input.send("nautilus".to_string());
+        // user_input.advance_to(2u64);
+        worker.step();
     })
     .unwrap();
 }
